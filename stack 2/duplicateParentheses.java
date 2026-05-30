bae45e2aec117af751e80bb3f410e1556b58c725
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-05-30 16:49:29 +0530

    add

---
+++ stack 2/duplicateParentheses.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class duplicateParentheses {
+
+    public static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // closing bracket
+            if (ch == ')') {
+
+                int count = 0;
+
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+
+                s.pop(); // remove '('
+
+                if (count < 1) {
+                    return true; // duplicate
+                }
+
+            } else {
+                // opening bracket or character/operator
+                s.push(ch);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+
+        String str = "((a+b))"; // true
+        String str2 = "(a-b)";  // false
+
+        System.out.println(isDuplicate(str));
+        System.out.println(isDuplicate(str2));
+    }
+}
