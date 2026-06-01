64ef9c46d7288b6c3c842324761171e9259d0920
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-06-01 14:58:53 +0530

    add

--- Queue/queueA.java
+++ Queue/queueA.java
@@ -1,50 +1,51 @@
 public class queueA {
     static class Queue{
-        static int arr[];
-        static int size;
-        static int rear;
+        int arr[];
+        int size;
+        int rear;
 
         Queue(int n){
             arr = new int[n];
             size = n;
-            rear =-1;
+            rear = -1;
         }
-        public static boolean isEmpty(){
+
+        public boolean isEmpty(){
             return rear == -1;
         }
 
-        //add
-        public static void add(int data){
-            if(rear == size-1){
+        // add
+        public void add(int data){
+            if(rear == size - 1){
                 System.out.println("Queue is full");
                 return;
             }
             rear = rear + 1;
             arr[rear] = data;
         }
-        //remove
-        public static int remove(){
+
+        // remove
+        public int remove(){
             if(isEmpty()){
                 System.out.println("empty queue");
                 return -1;
             }
 
             int front = arr[0];
-            for(int i =0; i<rear; i++){
-                arr[i] = arr[i+1];
+            for(int i = 0; i < rear; i++){
+                arr[i] = arr[i + 1];
             }
-            rear = rear -1;
+            rear = rear - 1;
             return front;
-
         }
-        //peek
-        public static int peek(){
+
+        // peek
+        public int peek(){
             if(isEmpty()){
                 System.out.println("empty queue");
                 return -1;
             }
             return arr[0];
-
         }
     }
     public static void main(String[] args) {
@@ -52,12 +53,11 @@ public class queueA {
         q.add(1);
         q.add(2);
         q.add(3);
-        //1`2 3
+        //1 2 3
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
         }
-        
     }
 }
