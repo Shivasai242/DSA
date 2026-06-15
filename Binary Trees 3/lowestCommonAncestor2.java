ea7cb0324314bddef593262b7598fe0d9e468278
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-06-15 19:00:18 +0530

    add

---
+++ Binary Trees 3/lowestCommonAncestor2.java
@@ -0,0 +1,53 @@
+public class lowestCommonAncestor2 {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2) { //O(n)
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca(root.left, n1, n2);
+        Node rightLca = lca(root.right, n1, n2);
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 1
+               /   \
+              2     3
+             / \   / \
+            4   5 6   7
+        */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6); 
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 7;
+
+        System.out.println("LCA = " + lca(root, n1, n2).data);
+    }
+}
