994cb08a793f5bafe1a3c6c52b6a89babcdf36a3
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-06-20 13:07:35 +0530

    add

---
+++ Binary Search Trees 1/rootToLeafPath.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class RootToLeafPath {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println("NULL");
+    }
+
+    public static void printRootToLeaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+
+        printRootToLeaf(root.left, path);
+        printRootToLeaf(root.right, path);
+
+        path.remove(path.size() - 1); // Backtracking
+    }
+
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 6, 10, 11, 14};
+
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+
+
+        printRootToLeaf(root, new ArrayList<>());
+    }
+}
--- Binary Search Trees 1/validateBST.java
+++ Binary Search Trees 1/validateBST.java
@@ -1,4 +1,4 @@
-public class PrintRange {
+public class validateBST {
 
     static class Node {
         int data;
@@ -22,7 +22,7 @@ public class PrintRange {
 
         return root;
     }
-    public static void inorder(Node root){
+        public static void inorder(Node root){
         if(root == null){
             return;
         }
@@ -31,38 +31,40 @@ public class PrintRange {
         inorder(root.right);
 
     }
-
-    public static void printInRange(Node root, int k1, int k2) {
-        if (root == null) {
-            return;
+    public static boolean isValidBST(Node root, Node min, Node max){
+        if(root == null){
+            return true;
         }
-
-        if (root.data >= k1 && root.data <= k2) {
-            printInRange(root.left, k1, k2);
-            System.out.print(root.data + " ");
-            printInRange(root.right, k1, k2);
-        } 
-        else if (root.data < k1) {
-            printInRange(root.right, k1, k2);
-        } 
-        else {
-            printInRange(root.left, k1, k2);
+        if(min != null && root.data <= min.data){
+            return false;
         }
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+        return isValidBST(root.left, min, root)
+               && isValidBST(root.right, root, max);
+
     }
 
     public static void main(String[] args) {
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        int values[] = {8, 5, 3, 6, 10, 11, 14};
+        //int  values[] = {1, 1, 1};
 
         Node root = null;
 
-         for(int i=0; i<values.length; i++){
+        for(int i=0; i<values.length; i++){
             root = insert(root, values[i]);
         }
 
         inorder(root);
         System.out.println();
 
-        System.out.print("Nodes in range 5 to 12: ");
-        printInRange(root, 5, 12);
+        if(isValidBST(root, null, null)){
+            System.out.println("valid");
+        }
+        else{
+            System.out.println("not valid");
+        }
     }
 }
+    
