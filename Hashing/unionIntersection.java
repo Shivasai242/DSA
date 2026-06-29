b46352655bf00fb3e442326d69707334c2791de2
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-06-29 20:14:59 +0530

    add

---
+++ Hashing/unionIntersection.java
@@ -0,0 +1,34 @@
+public class unionIntersection {
+    public static void main(String[] args) {
+        int arr1[] = {7, 3, 9};
+        int arr2[] = {6, 3, 9, 2, 9, 4};
+
+        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+
+        //union
+        for(int i=0; i<arr1.length; i++){
+            set.add(arr1[i]);
+        }
+        for(int i=0; i<arr2.length; i++){
+            set.add(arr2[i]);
+        }
+
+        System.out.println("Union = " + set);
+
+        //Intersection
+        set.clear();
+        for(int i=0; i<arr1.length; i++){
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+        for(int i=0; i<arr2.length; i++){
+            if(set.contains(arr2[i])){
+            count++;
+            set.remove(arr2[i]);
+            }
+        }
+        System.out.println("intersection  = " + count);
+    }
+    
+}
