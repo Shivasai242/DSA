b906ff111063f1d1367fe69c2bf417bb3f53d6c9
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-07-30 16:03:47 +0530

    add

--- Graphs 2/hello.java
+++ Graphs 2/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-            System.out.println("i love you 12345");
+            System.out.println("i love you 123456");
 
         
     }
