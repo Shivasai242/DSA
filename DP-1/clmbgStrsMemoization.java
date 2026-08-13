cb2da88cace5e472b7fa0cbb287265d474e30234
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-08-13 10:45:18 +0530

    add

---
+++ DP-1/clmbgStrsMemoization.java
@@ -0,0 +1,30 @@
+public class clmbgStrsMemoization {
+    //memoization
+        public static int countWays(int n, int ways[]){
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(ways[n] != -1){
+            //already calculated
+            return ways[n];
+        }
+        ways[n] = countWays(n-1, ways) + countWays(n-2, ways);
+        return ways[n];
+    }
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String args[]){
+        int n =5; //n=3 -> 3 & n=4 -> 5 => 8
+        int ways[] = new int[n+1]; 
+        Arrays.fill(ways, -1);
+        System.out.println(countWays(n, ways));
+    }
+    
+}
---
+++ DP-1/clmbgStrsTabulation.java
@@ -0,0 +1,18 @@
+public class clmbgStrsTabulation {
+    public static int countWaysTab(int n){
+        int dp[] = new int[n+1]; // 0 to n
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        int n = 5; // n=3 -> 3, n=4 -> 5, n=5 -> 8
+        System.out.println(countWaysTab(n));
+    }
+}
