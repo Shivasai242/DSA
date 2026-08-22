da1cc5581a54752a1f34d66fdc341f2990e74662
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-08-22 13:17:24 +0530

    add

--- Graphs 2/hello.java
+++ Graphs 2/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-            System.out.println("TKR college engineering");
+            System.out.println("egg rice");
 
         
     }
