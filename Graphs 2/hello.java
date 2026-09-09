352eb7433c71094d11ec81517711f2c8fc531362
Shivasai242 <shivasaigoudbairi@gmail.com>
2026-09-09 15:15:48 +0530

    add

--- Graphs 2/hello.java
+++ Graphs 2/hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-            System.out.println("rought book");
+            System.out.println("dont't trust anyone");
 
         
     }
